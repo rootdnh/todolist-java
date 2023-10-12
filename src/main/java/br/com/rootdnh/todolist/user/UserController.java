8f8cf6f3485643ff8a1e2bbb95d603b848e3b497
rootdnh <rootdnh@gmail.com>
2023-10-12 15:30:19 -0300

    chore: adding BCrypt in user controller

--- src/main/java/br/com/rootdnh/todolist/user/UserController.java
+++ src/main/java/br/com/rootdnh/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -23,7 +25,11 @@ public class UserController {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já exste");
     }
 
-    UserModel userCreated  = this.userRepository.save(userModel);
+    var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashed);
+
+    var userCreated  = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
